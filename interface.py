af18632a4592a7607946f89efaae57abbd93d40a
Lucas Carvalho <lcluz@sga.pucminas.br>
2024-06-14 12:27:25 -0300

    .

--- interface.py
+++ interface.py
@@ -1,22 +1,16 @@
 # Built-in libraries
 import io
 import tkinter as tk
-from tkinter import filedialog, ttk, messagebox
-
-# Third-party libraries
+from tkinter import font as tkfont
+import customtkinter as ctk
+from tkinter import filedialog, messagebox
 import cv2
-import joblib
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib.widgets import Button
 from PIL import Image, ImageTk
-import skimage
 from skimage.feature import graycomatrix, graycoprops
-from sklearn.preprocessing import StandardScaler
-
 import torch
 from torch import nn
-from torchvision import transforms
 from torchvision.transforms import ToTensor, Normalize, Compose
 from efficientnet_pytorch import EfficientNet
 
@@ -26,12 +20,8 @@ class CustomEfficientNet(nn.Module):
     def __init__(self, num_classes):
         super(CustomEfficientNet, self).__init__()
         self.efficient_net = EfficientNet.from_pretrained('efficientnet-b0')
-        
-        # Get the output dimension of the EfficientNet feature extractor
         efficient_net_output_dim = self.efficient_net._fc.in_features
         self.efficient_net._fc = nn.Identity()  # Remove the final classification layer
-
-        # Define the combined fully connected layer
         self.fc_combined = nn.Linear(efficient_net_output_dim, num_classes)
 
     def forward(self, images):
@@ -44,34 +34,41 @@ class ImageViewerApp:
     def __init__(self, master):
         self.master = master
         self.master.title("Image Viewer")
+        self.master.geometry("800x600")
+        self.master.resizable(True, True)
 
-        self.frame = tk.Frame(self.master)
-        self.frame.pack(fill=tk.BOTH, expand=True)
+        self.frame = ctk.CTkFrame(self.master)
+        self.frame.pack(fill=ctk.BOTH, expand=True)
 
-        self.canvas = tk.Canvas(self.frame)
-        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.canvas = ctk.CTkCanvas(self.frame)
+        self.canvas.pack(side=ctk.LEFT, fill=ctk.BOTH, expand=True)
         
-        self.image_frame = tk.Frame(self.canvas)
-        self.canvas.create_window((0, 0), window=self.image_frame, anchor=tk.NW)
+        self.image_frame = ctk.CTkFrame(self.canvas)
+        self.canvas.create_window((0, 0), window=self.image_frame, anchor=ctk.NW)
 
-        self.image_label = tk.Label(self.image_frame)
+        self.image_label = ctk.CTkLabel(self.image_frame)
         self.image_label.pack()
             
-        self.scrollbarVertical = ttk.Scrollbar(self.frame, orient=tk.VERTICAL, command=self.canvas.yview)
-        self.scrollbarVertical.pack(side=tk.RIGHT, fill=tk.Y)
+        self.scrollbarVertical = ctk.CTkScrollbar(self.frame, orientation=ctk.VERTICAL, command=self.canvas.yview)
+        self.scrollbarVertical.pack(side=ctk.RIGHT, fill=ctk.Y)
         self.canvas.configure(yscrollcommand=self.scrollbarVertical.set)
 
-        self.scrollbarHorizontal = ttk.Scrollbar(self.master, orient=tk.HORIZONTAL, command=self.canvas.xview)
-        self.scrollbarHorizontal.pack(side=tk.BOTTOM, fill=tk.X)
+        self.scrollbarHorizontal = ctk.CTkScrollbar(self.master, orientation=ctk.HORIZONTAL, command=self.canvas.xview)
+        self.scrollbarHorizontal.pack(side=ctk.BOTTOM, fill=ctk.X)
         self.canvas.configure(xscrollcommand=self.scrollbarHorizontal.set)
 
-        self.button_frame = tk.Frame(self.master)
+        self.button_frame = ctk.CTkFrame(self.master)
         self.button_frame.pack()
 
-        self.create_buttons()
+        self.button_info = self.create_buttons()
+        
         self.image_pil_base = None
         self.co_matrices = None
         self.current_matrix_index = 0
+        
+        # # Bind events for zoom buttons
+        # self.canvas.bind("<MouseWheelUp>", self.zoom_plus)
+        # self.canvas.bind("<MouseWheelDown>", self.zoom_minus)
 
     def create_buttons(self):
         button_info = [
@@ -90,10 +87,12 @@ class ImageViewerApp:
         ]
 
         for text, command in button_info:
-            state = "disabled" if text != "Open Image" else "normal"
-            btn = tk.Button(self.button_frame, text=text, command=command, width=10, height=2, state=state)
-            btn.pack(side=tk.LEFT)
+            state = "disabled" if text != "Open Image" else "active"
+            btn = ctk.CTkButton(self.button_frame, text=text, command=command, width=100, height=50, state=state)
+            btn.pack(side=ctk.LEFT)
             setattr(self, f"{text.replace(' ', '_').lower()}_button", btn)
+            
+        return button_info
 
     def place_graph(self, photo):
         self.canvas.delete("all")
@@ -107,7 +106,7 @@ class ImageViewerApp:
 
         self.canvas.create_image(x_offset, y_offset, anchor="nw", image=photo)
         self.canvas.image = photo
-        self.canvas.config(scrollregion=self.canvas.bbox("all"))    
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))    
 
     def place_image(self, image_pil):
         img_width = int(image_pil.width * zoom_factor)
@@ -129,13 +128,13 @@ class ImageViewerApp:
 
             self.place_image(self.image_pil_base)
             
-            self.open_image_button.config(text="Change Image")
+            self.open_image_button.configure(text="Change Image")
             self.enable_buttons()
 
     def enable_buttons(self):
         for attr in dir(self):
             if attr.endswith("_button"):
-                getattr(self, attr).config(state="active")
+                getattr(self, attr).configure(state="active")
 
     def zoom_plus(self):
         global zoom_factor
@@ -152,7 +151,6 @@ class ImageViewerApp:
 
     def revert_color(self):
         self.place_image(self.image_pil_base)
-        self.update_button_states(gray=False)
 
     def update_image(self, cvt_color_code, convert_method, mode):
         if self.image_pil_base:
@@ -161,37 +159,81 @@ class ImageViewerApp:
             matrix_converted = cvt_color_code == 'convert' and np.array(image_pil_rgb.convert(mode)) or cv2.cvtColor(matrix_rgb, cvt_color_code)
             image_pil_converted = Image.fromarray(matrix_converted)
             self.place_image(image_pil_converted)
-            self.update_button_states(gray=True)
-
-    def update_button_states(self, gray=False):
-        state_gray = "disabled" if gray else "active"
-        state_color = "active" if gray else "disabled"
-        self.gray_scale_button.config(state=state_gray)
-        self.colored_button.config(state=state_color)
 
     def convert_to_histogram_gray(self):
-        self.show_histogram('L', "Histograma de Tons de Cinza", [0, 16])
+        # Load and process the image
+        image = self.image_pil_base.convert('L')
+        image_16 = image.point(lambda p: p // 16 * 16)
+        histogram = image_16.histogram()
+        histogram_16 = [sum(histogram[i:i + 16]) for i in range(0, 256, 16)]
 
+        # Plot the histogram
+        fig, ax = plt.subplots()
+        ax.bar(range(16), histogram_16, width=1, edgecolor='black')
+
+        # Set the x and y limits to start from zero
+        ax.set_xlim(left=-1, right=16)
+        ax.set_ylim(bottom=0)
+
+        # Label the axes and set the title
+        ax.set_xlabel('Shade of Gray (0-15)')
+        ax.set_ylabel('Frequency')
+        ax.set_title('Histogram of Grayscale Image with 16 Shades')
+
+        # Set x-ticks to be from 0 to 15
+        ax.set_xticks(range(16))
+        ax.set_xticklabels(range(16))
+
+        # Show the plot
+        plt.show()
+    
     def convert_to_histogram_hsv(self):
-        self.show_histogram('HSV', "Histograma HSV", [0, 256, 0, 256])
+        # Convert the image to HSV
+        image_hsv = self.image_pil_base.convert('HSV')
+        image_hsv_np = np.array(image_hsv)
 
-    def show_histogram(self, mode, title, hist_range):
-        if self.image_pil_base:
-            image = self.image_pil_base.convert(mode)
-            array = np.array(image)
-            hist = cv2.calcHist([array], [0], None, [16], hist_range)
+        # Separate the channels
+        h_channel = image_hsv_np[:, :, 0]
+        s_channel = image_hsv_np[:, :, 1]
+        v_channel = image_hsv_np[:, :, 2]
 
-            plt.figure(figsize=(12, 7))
-            plt.title(title)
-            plt.plot(hist)
-            plt.xlim([0, 15])
+        # Calculate histograms for each channel
+        hist_h = np.histogram(h_channel, bins=16, range=(0, 256))[0]
+        hist_s = np.histogram(s_channel, bins=16, range=(0, 256))[0]
+        hist_v = np.histogram(v_channel, bins=16, range=(0, 256))[0]
 
-            buf = io.BytesIO()
-            plt.savefig(buf, format='png')
-            buf.seek(0)
-            img = Image.open(buf)
-            tk_img = ImageTk.PhotoImage(img)
-            self.place_graph(tk_img)
+        # Plot the histograms
+        fig, axs = plt.subplots(3, 1, figsize=(10, 8))
+
+        axs[0].bar(range(16), hist_h, width=1, edgecolor='black')
+        axs[0].set_xlim(left=-1, right=16)
+        axs[0].set_ylim(bottom=0)
+        axs[0].set_xlabel('Hue (0-15)')
+        axs[0].set_ylabel('Frequency')
+        axs[0].set_title('Histogram of Hue Channel')
+
+        axs[1].bar(range(16), hist_s, width=1, edgecolor='black')
+        axs[1].set_xlim(left=-1, right=16)
+        axs[1].set_ylim(bottom=0)
+        axs[1].set_xlabel('Saturation (0-15)')
+        axs[1].set_ylabel('Frequency')
+        axs[1].set_title('Histogram of Saturation Channel')
+
+        axs[2].bar(range(16), hist_v, width=1, edgecolor='black')
+        axs[2].set_xlim(left=-1, right=16)
+        axs[2].set_ylim(bottom=0)
+        axs[2].set_xlabel('Value (0-15)')
+        axs[2].set_ylabel('Frequency')
+        axs[2].set_title('Histogram of Value Channel')
+
+        # Set x-ticks to be from 0 to 15 for all subplots
+        for ax in axs:
+            ax.set_xticks(range(16))
+            ax.set_xticklabels(range(16))
+
+        # Show the plot
+        plt.tight_layout()
+        plt.show()
 
     def co_occurrence_matrices(self):
         if self.image_pil_base:
@@ -221,10 +263,8 @@ class ImageViewerApp:
     def haralick_descriptors(self, co_matrix):
         contrast = graycoprops(co_matrix, 'contrast')[0, 0]
         homogeneity = graycoprops(co_matrix, 'homogeneity')[0, 0]
-
         co_matrix_normed = co_matrix / np.sum(co_matrix)
         entropia = -np.sum(co_matrix_normed * np.log2(co_matrix_normed + (co_matrix_normed == 0)))
-
         return contrast, homogeneity, entropia
 
     def get_haralick_descriptors(self):
@@ -232,11 +272,9 @@ class ImageViewerApp:
             gray_img = self.image_pil_base.convert('L').resize((256, 256))
             gray_array = np.array(gray_img)
             gray_array = (gray_array / 16).astype(int)
-
             distances = [1, 2, 4, 8, 16, 32]
             features = [self.haralick_descriptors(graycomatrix(gray_array, [dist], [0], levels=16, symmetric=True, normed=True)) for dist in distances]
             features_str = "\n\n".join([f"Distância {dist}:\n  Contraste: {f[0]:.4f}\n  Homogeneidade: {f[1]:.4f}\n  Entropia: {f[2]:.4f}" for dist, f in zip(distances, features)])
-            
             messagebox.showinfo("Haralick Descriptors", features_str)
             
     def hu_invariants(self):
@@ -244,66 +282,85 @@ class ImageViewerApp:
             gray_img = np.array(self.image_pil_base.convert('L'))
             hsv_img = cv2.cvtColor(np.array(self.image_pil_base), cv2.COLOR_RGB2HSV)
             channels = [gray_img, hsv_img[:,:,0], hsv_img[:,:,1], hsv_img[:,:,2]]
-
+            channel_labels = ["Gray", "Hue", "Saturation", "Value"]
             hu_moments = [cv2.HuMoments(cv2.moments(ch)).flatten() for ch in channels]
-            hu_moments_str = "\n".join([f"Hu Moment {i+1} ({label}): {moment:.4e}" for label, moments in zip(["Gray", "Hue", "Saturation", "Value"], hu_moments) for i, moment in enumerate(moments)])
             
-            messagebox.showinfo("Hu Invariants", hu_moments_str)
+            hu_moments_str = ""
+            for label, moments in zip(channel_labels, hu_moments):
+                hu_moments_str += f"Hu Moments for {label} Channel:\n"
+                hu_moments_str += "\n".join([f"Hu Moment {i+1}: {moment:.4e}" for i, moment in enumerate(moments)])
+                hu_moments_str += "\n\n"
+            
+            # Create a Toplevel window
+            top = tk.Toplevel()
+            top.title("Hu Invariants")
+            
+            # Create a Text widget with a larger font
+            text_widget = tk.Text(top, wrap='word', padx=10, pady=10)
+            large_font = tkfont.Font(size=14)  # Adjust the size as needed
+            text_widget.configure(font=large_font)
+            
+            # Center the text
+            text_widget.tag_configure("center", justify='center')
+            text_widget.insert('1.0', hu_moments_str)
+            text_widget.tag_add("center", "1.0", "end")
+            text_widget.pack(expand=True, fill='both')
+            
+            # Add a scrollbar
+            scrollbar = tk.Scrollbar(top, command=text_widget.yview)
+            text_widget['yscrollcommand'] = scrollbar.set
+            scrollbar.pack(side='right', fill='y')
+
+            # Add an OK button to close the window
+            button = tk.Button(top, text="OK", command=top.destroy)
+            button.pack()
             
     def eficinetBinaryClassification(self):
         preprocess = Compose([
             ToTensor(),
             Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
-
         image = self.image_pil_base.convert('RGB')
         input_tensor = preprocess(image).unsqueeze(0)
-
+        state_dict = torch.load('custom_eficinet_model_binary.pth')
+        state_dict['fc_combined.weight'] = state_dict['fc_combined.weight'][:2, :]
+        state_dict['fc_combined.bias'] = state_dict['fc_combined.bias'][:2]
         model = CustomEfficientNet(num_classes=2)
-        model.load_state_dict(torch.load('custom_eficinet_model_binary.pth'))  # Carregar o modelo treinado
-        model.eval()
-
+        model.load_state_dict(state_dict)
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model.to(device)
         input_tensor = input_tensor.to(device)
-
         with torch.no_grad():
             output = model(input_tensor)
-
         class_labels = ["Normal", "Cancer"]
         _, predicted_idx = torch.max(output, 1)
         predicted_label = class_labels[predicted_idx.item()]
-
-        messagebox.showinfo("Classification", f"Prediction: {predicted_label}")
+        messagebox.showinfo("Classification", f"Possible:{class_labels}\n" + f"\nPrediction: {predicted_label}")
             
     def eficinetMultiClassification(self):
         preprocess = Compose([
             ToTensor(),
             Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
-
         image = self.image_pil_base.convert('RGB')
         input_tensor = preprocess(image).unsqueeze(0)
-
         model = CustomEfficientNet(num_classes=6)
-        model.load_state_dict(torch.load('custom_eficinet_model_6_categories.pth'))  # Carregar o modelo treinado
+        model.load_state_dict(torch.load('custom_eficinet_model_6_categories.pth'))
         model.eval()
-
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model.to(device)
         input_tensor = input_tensor.to(device)
-
         with torch.no_grad():
             output = model(input_tensor)
-
         class_labels = ['ASC-H', 'ASC-US', 'HSIL', 'LSIL', 'Normal', 'SCC']
         _, predicted_idx = torch.max(output, 1)
         predicted_label = class_labels[predicted_idx.item()]
-
-        messagebox.showinfo("Classification", f"Prediction: {predicted_label}")
+        messagebox.showinfo("Classification", f"Possible:{class_labels}\n" + f"\nPrediction: {predicted_label}")
 
 def main():
-    root = tk.Tk()
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("blue")
+    root = ctk.CTk()
     app = ImageViewerApp(root)
     root.mainloop()
 
